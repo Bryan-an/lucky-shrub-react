720a45bf3c1968f60889c787dde24d1005464919
Bryan Andagoya <bryanandagoya@gmail.com>
2025-03-07 23:38:29 -0500

    fix: remove background from Layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ import Footer from './Footer';
 
 const Layout: React.FC = () => {
   return (
-    <div className="min-h-screen bg-light font-sans text-dark">
+    <div className="min-h-screen font-sans text-dark">
       <div className="mx-auto flex min-h-screen max-w-[900px] flex-col px-4 py-4 pb-8 md:px-8 md:py-8 md:pb-12">
         <Header />
         <Navbar />
