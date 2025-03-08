41a342ae886abc0168828cfd7e424f83f91d59c3
Bryan Andagoya <bryanandagoya@gmail.com>
2025-03-07 23:45:34 -0500

    fix: remove references to deleted styles.css file

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Products from './pages/Products';
-import './styles.css';
 
 const router = createBrowserRouter([
   {
